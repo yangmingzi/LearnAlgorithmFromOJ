93f604f9f53fa8ce77b53555d536fadd92919db6
yangmingzi <mystudyingspace@163.com>
2015-02-02 23:59:28 +0800

    commit

--- POJ1753.cpp
+++ POJ1753.cpp
@@ -1,3 +1,4 @@
+//这种算法141MS,196K
 #include<iostream>
 using namespace std;
 int chess[17];
@@ -58,3 +59,133 @@ int main()
 	}
 	return 0;
 }
+
+/*
+//这种算法47ms,1088K
+#include <iostream>
+#include <cstring>
+ 
+#define count 16
+#define rwcount 4
+#define bitwidth 16
+#define maxlength 65536
+#define copyarr(source,dest) dest = source;
+ 
+typedef unsigned short round_status;
+typedef unsigned int uint;
+ 
+using namespace std;
+ 
+bool used[65536];
+ 
+void output(int steps)
+{
+       cout << steps;
+}
+ 
+void init(round_status &pstatus)
+{
+	memset(used, 0, sizeof(used));
+	pstatus = 0;
+	char c;
+	for (uint i = 0; i < count; ++i)
+	{
+		do
+                {
+                	cin >> c;
+                }
+                while (!(c == 'w' || c == 'b'));
+		if (c == 'b')
+			pstatus = pstatus | (1 << (15 - i));
+ 
+       }
+}
+ 
+void flip(round_status &pstatus, int x, int y)
+{
+	pstatus = pstatus ^ (1 << (bitwidth - (x * rwcount + y) - 1));
+	if (x > 0)
+	{
+		pstatus = pstatus ^ (1 << (bitwidth - ((x - 1) * rwcount + y) - 1));
+	}
+	if (y > 0)
+	{
+		pstatus = pstatus ^ (1 << (bitwidth - (x * rwcount + y - 1) - 1));
+	}
+	if (x < rwcount - 1)
+	{
+		pstatus = pstatus ^ (1 << (bitwidth - ((x + 1) * rwcount + y) - 1));
+	}
+	if (y < rwcount - 1)
+	{
+		pstatus = pstatus ^ (1 << (bitwidth - (x * rwcount + y + 1) - 1));
+	}
+}
+ 
+bool check(round_status pstatus)
+{
+	return (pstatus == 0 || (~pstatus) == 0 || pstatus == 65535);
+}
+ 
+bool hassame(round_status status)
+{
+	return used[status];
+}
+ 
+// Find the result. 
+void search(round_status &pstatus)
+{
+	round_status status[maxlength];
+	int step[maxlength];
+	int pf = 0, pc = 1;
+	step[0] = 0;
+	status[0] = pstatus;
+	used[pstatus] = true;
+	do
+	{
+		for (uint i = 0; i < rwcount; ++i)
+		{
+			for (uint j = 0; j < rwcount; ++j)
+			{
+                       		status[pc] = status[pf];
+				flip(status[pc], i, j);
+				if (hassame(status[pc]))
+				{
+					continue;
+				}
+				step[pc] = step[pf] + 1;
+				used[status[pc]] = true;
+				if (check(status[pc]))
+				{
+					output(step[pf] + 1);
+					return;
+				}
+				++pc;
+				if (pc > maxlength - 1)
+				{
+					cout << "Impossible";
+					return;
+				}
+			}
+		}
+		++pf;
+ 
+	}
+	while ((pf < pc));
+	cout << "Impossible";
+	return;
+}
+ 
+int main(void)
+{
+	round_status start_status; // start status of the field.
+	init(start_status);
+	if (check(start_status))
+	{
+		output(0);
+		return 0;
+	}
+	search(start_status);
+	return 0;
+}
+*/
